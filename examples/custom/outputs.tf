ec26ef406c0d532c70cdb0304f28b7a943625069
james-bailey <james.bailey@fisglobal.com>
2022-01-20 17:48:30 +0000

    First release fixes

--- examples/custom/outputs.tf
+++ examples/custom/outputs.tf
@@ -1,3 +1,4 @@
 output "api_group" {
+  description = "The Kubernets API Group that has been created"
   value = module.jetstack_certmanager.api_group
 }
--- variables.tf
+++ variables.tf
@@ -1,19 +1,23 @@
 variable "api_group" {
   description = "The name of the API group to create"
+  type = string
   default     = "cert-manager.io"
 }
 
 variable "name" {
   description = "The name of the appliation being deployed"
+  type = string
   default     = "cert-manager"
 }
 
 variable "namespace" {
   description = "The namespace to deploy the appliation in"
+  type = string
   default     = "cert-manager"
 }
 
 variable "kubernetes_version" {
   description = "The kubernetes version the application is being deployed onto"
+  type = string
   default     = "v1.6.1"
 }
