edb6e7df89af555994eb138ea76b75de99ed7257
国服最坑开发 <goreycn@users.noreply.github.com>
2023-06-27 10:29:36 +0800

    Update variables.tf

--- examples/standard/variables.tf
+++ examples/standard/variables.tf
@@ -1,5 +1,9 @@
 variable "k8s_cluster_name" {
   description = "The name of the target Kubernetes Cluster"
   type        = string
+}
 
+variable "region" {
+  description = "The aws region"
+  type        = string
 }
